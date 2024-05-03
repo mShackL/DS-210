6de11b559bd62dbfb64ea5a23e67eeea6cef9de9
Denzel Loblack <meschachdloblack@gmail.com>
2024-05-02 21:19:23 -0400

    Removed closenness centrality and added betweenness centrality function

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,18 @@ fn main() {
     let sample_size = 1000;    // Setting sample size from specified text file
     let avg_dist = average_distance(&graph, sample_size);
     let max_dist = max_distance(&graph);
-    // let median_dist = median_distance(&graph);
+    let median_dist = median_distance(&graph);
     // let std_dev_dist = standard_deviation_distance(&graph);
-    // let c_centrality = closeness_centrality(&graph);
+    let b_centrality = betweenness_centrality(&graph);
 
     println!("Random sample of 1000 node pairs:\n ");
     println!("Average distance between all node pairs: {:.2}", avg_dist);
     println!("Maxium distance between all node pairs: {:2}", max_dist);
-    // println!("Median distance between all node pairs: {:.2}", median_dist);
+    println!("Median distance between all node pairs: {:.2}", median_dist);
     // println!("Standard deviation of distance between all node pairs: {:.2}", std_dev_dist);
-    // for (node, centrality) in c_centrality.iter() {
-    //     println!("The closeness centrality of node {}: {:.2}", node, centrality);
-    // }
+    for (node, centrality) in b_centrality.iter() {
+        println!("The betweenness centrality of node {}: {:.2}", node, centrality);
+    }
 }
 
 use std::collections::{HashMap, HashSet};
@@ -139,25 +139,51 @@ fn max_distance(graph: &HashMap<String, Vec<String>>) -> usize {
 
 
 // Function to find the median distance between all pairs of nodes
-// fn median_distance(graph: &HashMap<String, Vec<String>>) -> f64 {
-//     let mut distances: Vec<usize> = Vec::new();
+fn median_distance(graph: &HashMap<String, Vec<String>>) -> f64 {
+    let nodes = graph.keys().collect::<Vec<_>>();
+    let num_nodes = nodes.len();
 
-//     for (start_node, _) in graph.iter() {
-//         for (target_node, _) in graph.iter() {
-//             if start_node != target_node {
-//                 let distance = bfs_distance(&graph, start_node, target_node);
-//                 distances.push(distance);
-//             }
-//         }
-//     }
-//     distances.sort_unstable();
-//     let len = distances.len();
-//     if len % 2 == 0 {
-//         (distances[len / 2 - 1] + distances[len / 2]) as f64 / 2.0
-//     } else {
-//         distances[len / 2] as f64
-//     }
-// }
+    let mut distances = vec![vec![usize::max_value(); num_nodes]; num_nodes];
+
+    for (i, node1) in nodes.iter().enumerate() {
+        if let Some(neighbors) = graph.get(*node1) {
+            for node2 in neighbors {
+                if let Some(j) = nodes.iter().position(|&x| x == node2) {
+                    distances[i][j] = 1; 
+                }
+            }
+        }
+    }
+
+    for k in 0..num_nodes {
+        for i in 0..num_nodes {
+            for j in 0..num_nodes {
+                if distances[i][k] != usize::max_value() && distances[k][j] != usize::max_value() {
+                    distances[i][j] = distances[i][j].min(distances[i][k] + distances[k][j]);
+                }
+            }
+        }
+    }
+
+    let mut all_distances = Vec::new();
+    for i in 0..num_nodes {
+        for j in 0..num_nodes {
+            if i != j && distances[i][j] != usize::max_value() {
+                all_distances.push(distances[i][j]);
+            }
+        }
+    }
+
+    all_distances.sort_unstable();
+
+
+    let len = all_distances.len();
+    if len % 2 == 0 {
+        (all_distances[len / 2 - 1] + all_distances[len / 2]) as f64 / 2.0
+    } else {
+        all_distances[len / 2] as f64
+    }
+}
 
 
 // fn standard_deviation_distance(graph: &HashMap<String, Vec<String>>) -> f64 {
@@ -179,33 +205,91 @@ fn max_distance(graph: &HashMap<String, Vec<String>>) -> usize {
 
 
 
-// fn closeness_centrality(graph: &HashMap<String, Vec<String>>) -> HashMap<String, f64> {
-//     let mut closeness_centralities: HashMap<String, f64> = HashMap::new();
 
-//     for (node, _) in graph.iter() {
-//         let mut total_distance = 0;
-//         let mut close_nodes_count = 0;
+#[derive(Clone, Debug)]
+struct Path {
+    nodes: Vec<String>,
+}
 
-//         for (other_node, _) in graph.iter() {
-//             if node != other_node {
-//                 let distance = bfs_distance(graph, node, other_node);
-//                 if distance != usize::max_value() {
-//                     total_distance += distance;
-//                     close_nodes_count += 1;
-//                 }
-//             }
-//         }
+impl Path {
+    fn new() -> Self {
+        Path { nodes: Vec::new() }
+    }
+}
 
-//         if close_nodes_count > 0 {
-//             let closeness_centrality = (close_nodes_count as f64) / (total_distance as f64);
-//             closeness_centralities.insert(node.clone(), closeness_centrality);
-//         } else {
-//             closeness_centralities.insert(node.clone(), 0.0);
-//         }
-//     }
+// Define a struct to hold the result of the betweenness centrality calculation
+struct BetweennessCentrality {
+    counts: HashMap<String, usize>,
+}
 
-//     closeness_centralities
-// }
+impl BetweennessCentrality {
+    fn new() -> Self {
+        BetweennessCentrality { counts: HashMap::new() }
+    }
+
+    fn add_node(&mut self, node: &str) {
+        let count = self.counts.entry(node.to_string()).or_insert(0);
+        *count += 1;
+    }
+
+    fn calculate_percentage(&mut self, num_paths: usize) {
+        for count in self.counts.values_mut() {
+            *count = (*count * 100) / num_paths;
+        }
+    }
+}
+
+// Function to calculate betweenness centrality
+fn betweenness_centrality(graph: &HashMap<String, Vec<String>>) -> HashMap<String, f64> {
+    let mut betweenness: HashMap<String, f64> = HashMap::new();
+    let mut all_paths: Vec<Path> = Vec::new();
+
+    // Calculate all shortest paths between node pairs
+    for start_node in graph.keys() {
+        let mut visited: HashSet<String> = HashSet::new();
+        let mut stack: Vec<(String, Path)> = Vec::new();
+
+        stack.push((start_node.clone(), Path::new()));
+
+        while let Some((current_node, mut path)) = stack.pop() {
+            if visited.contains(&current_node) {
+                continue;
+            }
+
+            visited.insert(current_node.clone());
+            path.nodes.push(current_node.clone());
+
+            if path.nodes.len() > 1 {
+                all_paths.push(path.clone());
+            }
+
+            if let Some(neighbors) = graph.get(&current_node) {
+                for neighbor in neighbors {
+                    stack.push((neighbor.clone(), path.clone()));
+                }
+            }
+        }
+    }
+
+    // Count the appearance of each node in the paths
+    let mut centrality = BetweennessCentrality::new();
+    let num_paths = all_paths.len();
+    for path in all_paths {
+        for node in path.nodes {
+            centrality.add_node(&node);
+        }
+    }
+
+    // Calculate the percentage for each node
+    centrality.calculate_percentage(num_paths);
+
+    // Store the results in the betweenness centrality HashMap
+    for (node, count) in centrality.counts {
+        betweenness.insert(node, count as f64);
+    }
+
+    betweenness
+}
 
 
 
